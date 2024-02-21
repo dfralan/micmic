7de88936f1d5f583a2775c1656f39c2460eca298
dfralan <dfralan@gmail.com>
2024-02-21 13:46:45 -0300

    commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare namespace astroHTML.JSX {
+    interface HTMLAttributes {
+      'fill-as-inner'?: number;
+      'loom'?: string;
+    }
+  }
