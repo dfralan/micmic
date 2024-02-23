0427dc7f78114aae0ea6e28f39f4f86b82635421
dfralan <dfralan@gmail.com>
2024-02-23 20:16:34 -0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import cloudflare from '@astrojs/cloudflare';
+import netlify from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: cloudflare()
+  adapter: netlify()
 });
