2a0b30f5d773f7a335cf6957394b6e01177a47f5
dfralan <dfralan@gmail.com>
2024-02-23 13:29:58 -0300

    added netlify adapter

--- netlify-config.js
+++ netlify-config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import netlify from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: netlify(),
 });
