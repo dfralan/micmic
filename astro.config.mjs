d907d76d423ee5fdfdf2abc471faaf92a5b86e6e
dfralan <dfralan@gmail.com>
2024-02-23 19:36:09 -0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
-const GoogleID = import.meta.env.GOOGLE_ID;
-const GoogleSecret = import.meta.env.GOOGLE_SECRET;
 import auth from 'auth-astro';
-import Google from "@auth/core/providers/google"
 
 export default defineConfig({
-  integrations: [tailwind(),
-  auth({
-    providers: [
-      Google({
-        clientId: GoogleID,
-        clientSecret: GoogleSecret
-      })
-    ]
-  })
-  ],
+  integrations: [tailwind(), auth()],
   output: 'server',
   adapter: cloudflare(),
 });
