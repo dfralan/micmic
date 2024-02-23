18aee069c1c424788944bdf862796b98ad94deaf
dfralan <dfralan@gmail.com>
2024-02-23 17:06:27 -0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import auth from 'auth-astro';
 
 export default defineConfig({
-  integrations: [auth(), tailwind()],
+  integrations: [tailwind()],
   output: 'server',
   adapter: cloudflare(),
 });
