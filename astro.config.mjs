563e7a327a8f778faf874538c1fe508d4cb1806a
dfralan <dfralan@gmail.com>
2024-02-23 16:54:58 -0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
+import auth from 'auth-astro';
 
 export default defineConfig({
   integrations: [tailwind()],
---
+++ src/assets/privacy.ts
@@ -0,0 +1,39 @@
+export default 
+  {
+    name: "Políticas de Privacidad",
+    description: "¿Cómo recopilamos tu información?",
+    content: `
+    <h1 class="text-2xl font-semibold">Política de Privacidad de micmic.shop</h1>
+    <p class='text-gray-700'>Última actualización: 23/02/2024</p>
+    <p>Gracias por visitar micmic.shop ("Sitio web"). En micmic, valoramos la privacidad de nuestros usuarios y nos comprometemos a proteger la información personal que puedan proporcionar mientras utilizan nuestro sitio web y nuestros servicios relacionados. Esta Política de Privacidad describe cómo recopilamos, utilizamos y compartimos su información cuando visita o realiza transacciones en micmic.shop.</p>
+    <h2 class='text-xl'>1. Información que recopilamos:</h2>
+    <p>Cuando visita micmic.shop, recopilamos cierta información sobre su dispositivo y su interacción con nuestro sitio web. Esto incluye información sobre su dirección IP, tipo de navegador, páginas visitadas, tiempo dedicado en nuestro sitio y otros datos de diagnóstico.</p>
+    <p>Además, cuando se registra para utilizar nuestros servicios, recopilamos información personal que nos proporciona directamente, como su nombre, dirección de correo electrónico y detalles de pago. Tenga en cuenta que no almacenamos datos sensibles de tarjetas de crédito o débito; todas las transacciones de pago se gestionan a través de nuestra plataforma de pago seguro.</p>
+    <h2 class='text-xl'>2. Uso de la información:</h2>
+    <p>Utilizamos la información recopilada para diversos fines, que incluyen:</p>
+    <ul>
+        <li>Proporcionar y mantener nuestro sitio web y servicios.</li>
+        <li>Personalizar y mejorar su experiencia en micmic.shop.</li>
+        <li>Comprender y analizar cómo interactúa con nuestro sitio web.</li>
+        <li>Comunicarnos con usted, ya sea para responder a consultas, proporcionar asistencia técnica, informarle sobre cambios en nuestros servicios o enviarle comunicaciones de marketing según su consentimiento.</li>
+        <li>Cumplir con nuestras obligaciones legales y prevenir posibles fraudes o actividades ilícitas.</li>
+    </ul>
+    <h2 class='text-xl'>3. Compartir información:</h2>
+    <p>No vendemos, alquilamos ni compartimos su información personal con terceros, excepto en las siguientes circunstancias:</p>
+    <ul>
+        <li>Con su consentimiento explícito.</li>
+        <li>Cuando sea necesario para cumplir con una obligación legal o para proteger nuestros derechos, propiedad o seguridad, así como los de nuestros usuarios u otros.</li>
+        <li>Con proveedores de servicios externos que nos ayudan a operar nuestro sitio web y brindar nuestros servicios, sujetos a acuerdos de confidencialidad adecuados.</li>
+    </ul>
+    <h2 class='text-xl'>4. Cookies y tecnologías similares:</h2>
+    <p>micmic.shop utiliza cookies y tecnologías similares para mejorar la funcionalidad y la experiencia del usuario en nuestro sitio web. Puede configurar su navegador para que rechace todas las cookies o para que le avise cuando se envíe una cookie. Sin embargo, si opta por desactivar las cookies, es posible que algunas partes de nuestro sitio web no funcionen correctamente.</p>
+    <h2>5. Seguridad de la información:</h2>
+    <p>Tomamos medidas razonables para proteger la información personal de nuestros usuarios contra accesos no autorizados, divulgación, alteración o destrucción. Sin embargo, tenga en cuenta que ningún método de transmisión a través de internet o almacenamiento electrónico es 100% seguro.</p>
+    <h2 class='text-xl'>6. Cambios en la Política de Privacidad:</h2>
+    <p>Nos reservamos el derecho de actualizar o modificar esta Política de Privacidad en cualquier momento. Le notificaremos cualquier cambio significativo mediante una notificación en nuestro sitio web o por correo electrónico antes de que el cambio entre en vigencia.</p>
+    <h2 class='text-xl'>7. Contacto:</h2>
+    <p>Si tiene alguna pregunta o inquietud sobre esta Política de Privacidad, puede ponerse en contacto con nosotros a través de [correo electrónico de contacto].</p>
+    <p>Al utilizar micmic.shop, usted acepta los términos de esta Política de Privacidad.</p>
+`
+  }
+;
