790bd065b7c946ebbd0b7c71387b06ec4ede8115
dfralan <dfralan@gmail.com>
2024-02-24 15:16:47 -0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
-import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://micmic.shop/', // this line is required
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind()],
   output: "server",
   adapter: netlify()
 });
