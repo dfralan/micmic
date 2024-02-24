209036ea7797a7054b7b2f77f273e5f40e999049
dfralan <dfralan@gmail.com>
2024-02-24 02:53:50 -0300

    commit

--- .netlify/functions-internal/ssr/chunks/_.._CF0Fe0_6.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__DSu_TdgH.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/login_DPcWxCfS.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/login_BWR5Qw1O.mjs');
-
-export { page };
