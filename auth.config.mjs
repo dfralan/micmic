c8c1c4a40e7aef1f876f00458f570a0481368049
dfralan <dfralan@gmail.com>
2024-02-24 02:47:04 -0300

    commit

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,9 +1,9 @@
-import google from '@auth/core/providers/google';
+import Google from '@auth/core/providers/google';
 import { defineConfig } from 'auth-astro';
 
 export default defineConfig({
   providers: [
-    google({
+    Google({
       clientId: import.meta.env.GOOGLE_CLIENT_ID,
       clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
     }),
