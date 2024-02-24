315ee4a013752c5aceac229b33b456b89008d305
dfralan <dfralan@gmail.com>
2024-02-24 09:30:18 -0300

    commit

--- .netlify/functions-internal/ssr/chunks/demo-432_6TKOTDUN.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/demo-432_CPQ4JQUD.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/privacy_B3yO9b1_.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/privacy_B8jci-jh.mjs');
-
-export { page };
